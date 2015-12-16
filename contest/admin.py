9ef8d348399352b46e27b89a0fe195979a252d20
Kamil <kamil.springer@gmail.com>
2015-12-16 23:15:31 +0100

    [feature/rush-21] added docstrings changed names of classes and functions

--- contest/admin.py
+++ contest/admin.py
@@ -1,3 +1,28 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from .models import RushUser
+
+
+class PersonAdmin(admin.ModelAdmin):
+    list_filter = ('is_active',)
+
+
+class RushUserAdmin(admin.ModelAdmin):
+    """What RushUserAdmin can do and how RushUser is displayed."""
+
+    def confirm(modeladmin, request, queryset):
+    	"""RushUserAdmin can confirm RushUser."""
+        queryset.update(status='Yes')
+
+    def cancel(modeladmin, request, queryset):
+    	"""RushUserAdmin can cancel RushUser."""
+        queryset.update(status='No')
+
+    exclude = (
+        'password', 'date_joined', 'last_login', 'status', 'is_superuser',
+        'is_staff', 'is_active',
+    )
+    list_display = ('first_name', 'last_name')
+    readonly_fields = ('last_login', 'date_joined')
+    actions = [confirm, cancel]
+admin.site.register(RushUser, RushUserAdmin)
