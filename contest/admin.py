8af9c35bd05bc6414e81aed1270e6ea91647eaba
Jakub Maslanka <Ocean.spokojny@op.pl>
2015-12-24 00:16:07 +0100

    [feature/rush-22] added functionality for buttons in user requests

--- contest/admin.py
+++ contest/admin.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from unidecode import unidecode
 from django.contrib import admin
-from contest.models import RushUser
+
+from .models import RushUser
+
+
+class PersonAdmin(admin.ModelAdmin):
+    """
+    It filters users by their is_active status
+    """
+    list_filter = ('is_active',)
 
 
 class RushUserAdmin(admin.ModelAdmin):
@@ -9,17 +18,28 @@ class RushUserAdmin(admin.ModelAdmin):
     What RushUserAdmin can do and how RushUser is displayed.
     """
 
-    def confirm(self, request, queryset):
+    def create(self, request, queryset):
         """
-        RushUserAdmin can confirm RushUser.
+        RushUserAdmin confirm RushUser requests - changing
+        accounts status to active and setting login name
         """
-        queryset.update(is_active=True)
+        for obj in queryset:
+            obj.is_active = True
+            name = unidecode(obj.first_name[0]).lower()
+            surname = unidecode(obj.last_name).lower()
+            obj.username = name + surname
+            obj.set_password('password123')
+            obj.save()
+    create.short_description = 'Stwórz konto'
 
     def cancel(self, request, queryset):
         """
-        RushUserAdmin can cancel RushUser.
+        RushUserAdmin can decline RushUser requests -
+        deleting accounts from database
         """
-        queryset.update(is_active=False)
+        if queryset.count():
+            queryset.delete()
+    cancel.short_description = 'Usuń'
 
     exclude = (
         'password', 'date_joined', 'last_login', 'status', 'is_superuser',
@@ -27,8 +47,7 @@ class RushUserAdmin(admin.ModelAdmin):
     )
     list_display = ('first_name', 'last_name')
     readonly_fields = ('last_login', 'date_joined')
-    actions = [confirm, cancel]
-    list_filter = ('is_active',)
+    actions = [create, cancel]
 
 
 admin.site.register(RushUser, RushUserAdmin)
--- contest/tests.py
+++ contest/tests.py
@@ -3,15 +3,19 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 from django.contrib.auth import authenticate
+from django.contrib.admin.sites import AdminSite
+from django.http import HttpRequest
 
 from contest.models import RushUser
 from contest.forms import LoginForm
+from contest.admin import RushUserAdmin
 
 
 class UserMethodTests(TestCase):
     def setUp(self):
         self.user = RushUser.objects.create_user(
-            'xyz@xyz.pl', 'Name', 'Last Name', 'School', 'Address', 'Password'
+            'xyz@xyz.pl', 'Name', 'Last Name', 'School', 'Address', 'Password',
+            'username'
         )
 
     def test_authenticate(self):
@@ -20,18 +24,18 @@ class UserMethodTests(TestCase):
         on input. In this case: correct data, incorrect, empty.
         """
         self.assertEqual(
-            authenticate(email='xyz@xyz.pl', password='Password'),
+            authenticate(username='username', password='Password'),
             self.user
         )
         self.assertEqual(
-            authenticate(email='xyz@xyz.pl', password='random_pass'),
+            authenticate(username='username', password='random_pass'),
             None
         )
         self.assertEqual(
-            authenticate(email='example@example.pl',password='qwerty'),
+            authenticate(username='example@example.pl', password='qwerty'),
             None
         )
-        self.assertEqual(authenticate(email='', password=''), None)
+        self.assertEqual(authenticate(username='', password=''), None)
 
     def test_login_form(self):
         """
@@ -40,18 +44,18 @@ class UserMethodTests(TestCase):
         """
         self.assertEqual(list(LoginForm.base_fields), ['username', 'password'])
 
-        form_data = {'username': 'xyz@xyz.pl', 'password': 'wrong_password'}
+        form_data = {'username': 'username', 'password': 'wrong_password'}
         form = LoginForm(data=form_data)
         self.assertFalse(form.is_valid())
         self.assertEqual(
             form.errors['__all__'],
             [
-                'Wprowadź poprawny adres email oraz hasło. '
+                'Wprowadź poprawny login oraz hasło. '
                 'Uwaga: wielkość liter ma znaczenie.'
             ]
         )
 
-        form_data = {'username': 'xyz@xyz.pl', 'password': 'Password'}
+        form_data = {'username': 'username', 'password': 'Password'}
         form = LoginForm(data=form_data)
         self.assertFalse(form.is_valid())
         self.assertEqual(
@@ -62,3 +66,42 @@ class UserMethodTests(TestCase):
         self.user.save()
         form = LoginForm(data=form_data)
         self.assertTrue(form.is_valid())
+
+
+class AdminMethodTests(TestCase):
+    def setUp(self):
+        self.obj_1 = RushUser.objects.create_user(
+            'aaa@aaa.pl', 'Łukasz', 'Ślązak', 'School', 'Address', 'Password',
+            'login1'
+        )
+        self.obj_2 = RushUser.objects.create_user(
+            'bbb@bbb.pl', 'Adam', 'Ślowacki', 'School', 'Address', 'Password',
+            'login2'
+        )
+        self.obj_3 = RushUser.objects.create_user(
+            'ccc@ccc.pl', 'Ewa', 'Kowalska', 'School', 'Address', 'Password',
+            'random_login'
+        )
+
+        self.request = HttpRequest()
+        self.app_admin = RushUserAdmin(RushUser, AdminSite())
+
+    def test_creating_user(self):
+        self.assertFalse(self.obj_1.is_active and self.obj_2.is_active)
+        self.assertEqual(self.obj_1.username, 'login1')
+        self.assertEqual(self.obj_2.username, 'login2')
+
+        queryset = [self.obj_1, self.obj_2]
+        self.app_admin.create(self.request, queryset)
+
+        self.assertTrue(self.obj_1.is_active and self.obj_2.is_active)
+        self.assertEqual(self.obj_1.username, 'lslazak')
+        self.assertEqual(self.obj_2.username, 'aslowacki')
+
+    def test_deleting_user(self):
+        self.assertTrue(RushUser.objects.filter(email='ccc@ccc.pl'))
+
+        queryset = RushUser.objects.filter(email='ccc@ccc.pl')
+        self.app_admin.cancel(self.request, queryset)
+
+        self.assertFalse(RushUser.objects.filter(email='ccc@ccc.pl'))
