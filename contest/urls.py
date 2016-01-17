10907d6bf78fcfd960c2fc2c526fbe6ca23b9dd8
Patryk <pixsel001@gmail.com>
2016-01-17 15:33:25 +0100

    [feature/rush-28] fix errors in ticket 28

--- contest/urls.py
+++ contest/urls.py
@@ -1,15 +1,19 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
 
 from contest import views
 
 
 urlpatterns = [
+    url(r'^$', login_required(views.HomeView.as_view()), name='home'),
+    url(r'^register/$', views.RegisterView.as_view(), name='register'),
     url(r'^$', views.HomeView.as_view(), name='home'),
     url(r'^login/$', views.LoginView.as_view(), name='login'),
     url(
         r'^administrator/konta/$',
-        views.AccountsView.as_view(), name='Accounts'
+        views.AccountsView.as_view(),
+        name='accounts'
         ),
     url(r'^logout/$', auth_views.logout_then_login, name='logout'),
 ]
--- contest/views.py
+++ contest/views.py
@@ -1,11 +1,51 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.views.generic import TemplateView, View
 
+from django.views.generic import (
+    TemplateView,
+    View,
+)
 from django.contrib.auth import login
-from django.shortcuts import render, redirect
-from contest.forms import LoginForm
-from .models import RushUser
+from django.shortcuts import (
+    redirect,
+    render,
+)
+
+from contest.forms import (
+    LoginForm,
+    RegistrationForm,
+)
+from contest.models import RushUser
+
+
+class RegisterView(View):
+    """
+    View for user registration.
+    """
+    form_class = RegistrationForm
+    template_name = 'contest/register.html'
+
+    def get(self, request, *args, **kwargs):
+        """
+        Return registration form on site.
+        """
+        form = self.form_class()
+
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        """
+        Send form and check validation.
+        """
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(
+                request, self.template_name,
+                {'success_message': 'Wysłano zapytanie o konto'}
+            )
+        else:
+            return render(request, self.template_name, {'form': form})
 
 
 class HomeView(TemplateView):
@@ -25,10 +65,18 @@ class LoginView(View):
     template_name = 'contest/login.html'
 
     def get(self, request):
+        """
+        Displaying clear LoginForm on page without any errors.
+        """
         form = self.form_class()
         return render(request, self.template_name, {'form': form})
 
     def post(self, request):
+        """
+        Checking if form is valid (correct login, password, account
+        activated). If everything is okay logs user in and redirects to home
+        page, if not - returns form with errors.
+        """
         form = self.form_class(request, data=request.POST)
         if form.is_valid():
             login(request, form.get_user())
@@ -42,8 +90,8 @@ class AccountsView(View):
     confirm or cancel, and fields 'imie' and 'nazwisko' RushUser.
     """
     def get(self, request):
-        posts = RushUser.objects.filter()
-        return render(
-                      request, 'contest/Accounts.html',
-                      {'posts': posts}
-                      )
+        """
+        Transfer RushUser model and render pages 'administratorzy/konta'.
+        """
+        users = RushUser.objects.filter()
+        return render(request, 'contest/accounts.html', {'users': users})
