2a82382666f8bc5bb61000c45d3fc4a9d706e2f9
Kamil <kamil.springer@gmail.com>
2016-01-26 23:44:09 +0100

    [feature/rush-19] created relation many to one and added checkbox

--- contest/forms.py
+++ contest/forms.py
@@ -15,14 +15,13 @@ class RegistrationForm(forms.ModelForm):
     """
     Form for new user registration.
     """
-
     class Meta:
         model = RushUser
         fields = [
             'email', 'first_name', 'last_name', 'organization_name',
-            'organization_address',
+            'organization_address', 'club',
         ]
-
+    
 
 class LoginForm(AuthenticationForm):
     """
