0b7b59b5874893f076a741b84c14217dc87f8a82
Piotr Bajsarowicz <bapiotr@gmail.com>
2016-03-13 19:19:28 +0100

    [feature/rush-30] minor review fixes

--- contest/views/generics.py
+++ contest/views/generics.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.core.mail import EmailMessage
 from django.conf import settings
+from django.core.mail import EmailMessage
 from django.forms import formset_factory
 from django.shortcuts import render
 from django.template import loader
