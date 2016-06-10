0fcc22f427b8c4276ee72f60b50ae8a1a1ba4ff9
Dominik Otulak <otulakdominik@gamil.com>
2016-06-10 18:38:08 +0200

    [feature/rush-79] contest order by date

--- contest/views/generics.py
+++ contest/views/generics.py
@@ -36,8 +36,10 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
-        context['upcoming'] = Contest.objects.filter(date__gte=timezone.now())
-        context['completed'] = Contest.objects.filter(date__lt=timezone.now())
+        context['upcoming'] = Contest.objects.filter(
+            date__gte=timezone.now()).order_by('date')
+        context['completed'] = Contest.objects.filter(
+            date__lt=timezone.now()).order_by('date')
 
         return context
 
