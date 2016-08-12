2e96659a9a41740b88a8bd2c42619ab269a78b91
kamilspringer <kamil.springer@gmail.com>
2016-08-12 21:52:58 +0200

    [feature/rush-88]nothing

--- contest/models.py
+++ contest/models.py
@@ -288,7 +288,7 @@ class ContestFiles(models.Model):
     date_uploaded = models.DateTimeField(auto_now_add=True)
     contest_file = models.FileField('Pliki', upload_to=contest_directory_path)
     url = models.CharField('Ścieżka do pliku', max_length=255, default='')
-    name = models.CharField('Ścieżka do pliku', max_length=255, default='')
+    name = models.CharField('Nazwa pliku', max_length=255, default='')
 
 
 class Contestant(models.Model):
